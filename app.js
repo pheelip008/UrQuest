3e9cc09c127e2e126c7a53f7521300575a7f4792
Pheelip <pheelipraipure@gmail.com>
2026-02-07 19:32:09 +0530

    delpoyed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
+    ? 'http://127.0.0.1:8000' 
+    : 'https://urquest-api.onrender.com'; 
 
 const app = {
     user: null, 
